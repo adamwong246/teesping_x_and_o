17b5317c8ba84e601fc888a4fdcd56a12878e83c
Adam Wong <adamwong246@gmail.com>
2014-06-17 20:54:59 -0700

    all functions staked out

--- test.rb
+++ test.rb
@@ -84,6 +84,9 @@ class AI
         if board[y][x] == 0
           mutated_board = Marshal.load(Marshal.dump(board))
           mutated_board[y][x] = key
+
+          # binding.pry
+
           mutated_winning_moves = winning_moves(mutated_board, key).uniq
           if mutated_winning_moves.count > 1
             forking_moves << mutated_winning_moves
@@ -97,8 +100,44 @@ class AI
   end
 
   def fork_blocking_moves(board, key)
-    winning_moves(board, key*-1).uniq
+    forking_moves(board, key*-1).uniq
   end
+
+  def center_moves(board, key)
+    if board[1][1] == 0
+      return [{x:1, y:1}]
+    else
+      []
+    end
+  end
+
+  def opposite_corner_moves(board, key)
+    moves = []
+    moves << {x:2, y:2} if board[0][0] == key * -1 && board[2][2] == 0
+    moves << {x:0, y:0} if board[2][2] == key * -1 && board[0][0] == 0
+    moves << {x:0, y:2} if board[0][2] == key * -1 && board[2][0] == 0
+    moves << {x:2, y:0} if board[2][0] == key * -1 && board[0][2] == 0
+    moves
+  end
+
+  def empty_corner_moves(board, key)
+    moves = []
+    moves << {x:2, y:2} if board[2][2] == 0
+    moves << {x:0, y:0} if board[0][0] == 0
+    moves << {x:0, y:2} if board[2][0] == 0
+    moves << {x:2, y:0} if board[0][2] == 0
+    moves
+  end
+
+  def empty_side_moves(board, key)
+    moves = []
+    moves << {x:0, y:1} if board[1][0] == 0
+    moves << {x:2, y:1} if board[1][2] == 0
+    moves << {x:1, y:2} if board[2][1] == 0
+    moves << {x:1, y:0} if board[0][1] == 0
+    moves
+  end
+
 
 end
 
@@ -128,13 +167,6 @@ describe AI, "#score" do
       AI.new.winning_moves([[0,  0,  1],
                             [0,  0,  0],
                             [1,  0,  0]], 1).should include({x:1, y:1})
-    end
-
-
-    it "should recognize a negative win" do
-      AI.new.winning_moves([[-1, -1,  0],
-                            [0,   0,  0],
-                            [0,   0,  0]], -1).should include({x:2, y:0})
     end
 
     it "should recognize a not-win" do
@@ -181,13 +213,6 @@ describe AI, "#score" do
                             [1,  0,  0]], -1).should include({x:1, y:1})
     end
 
-
-    it "should recognize a negative blocking_moves" do
-      AI.new.blocking_moves([[-1, -1,  0],
-                            [0,   0,  0],
-                            [0,   0,  0]], 1).should include({x:2, y:0})
-    end
-
     it "should recognize a not-blocking_moves" do
       AI.new.blocking_moves([[1, 0, 0],
                             [0, 0, 0],
@@ -211,9 +236,9 @@ describe AI, "#score" do
 
   describe "#fork_blocking_moves" do
     it "should recognize fork_blocking_moves" do
-      AI.new.fork_blocking_moves([[1,  0,  0],
-                                  [0,  0,  0],
-                                  [1,  0,  0]], -1).should include({x:2, y:0}, {x:1, y:1}, {x:2, y:2})
+      AI.new.fork_blocking_moves([[1,   0,  0],
+                                  [0,   0,  0],
+                                  [1,   0,  0]], -1).should include({x:2, y:0}, {x:1, y:1}, {x:2, y:2})
     end
 
     it "should recognize non-fork_blocking_moves" do
@@ -223,4 +248,84 @@ describe AI, "#score" do
     end
   end
 
+  describe "#center_moves" do
+    it "should return the center square if available" do
+      AI.new.center_moves([[0,  0,  0],
+                            [0,  0,  0],
+                            [0,  0,  0]], 1).should eq([{x:1, y:1}])
+    end
+
+    it "should recognize when the center square is taken" do
+     AI.new.center_moves([ [0,  0,  0],
+                            [0,  1,  0],
+                            [0,  0,  0]], 1).should be_empty
+    end
+
+    it "should recognize when the center square is taken" do
+     AI.new.center_moves([ [0,  0,   0],
+                            [0,  -1,  0],
+                            [0,  0,   0]], 1).should be_empty
+    end
+  end
+
+  describe "#opposite_corner_moves" do
+     it "should recognize the NE corner" do
+     AI.new.opposite_corner_moves([ [0,  0, -1],
+                                    [0,  0,  0],
+                                    [0,  0,  0]], 1).should  =~ [{x:0, y:2}]
+    end
+
+    it "should recognize the SE corner" do
+     AI.new.opposite_corner_moves([ [0,  0,  0],
+                                    [0,  0,  0],
+                                    [0,  0, -1]], 1).should =~ [{x:0, y:0}]
+    end
+    it "should recognize the SW corner" do
+     AI.new.opposite_corner_moves([ [0,  0, 0],
+                                    [0,  0, 0],
+                                    [-1, 0, 0]], 1).should =~ [{x:2, y:0}]
+    end
+
+    it "should recognize the NW corner" do
+     AI.new.opposite_corner_moves([ [-1,  0,   0],
+                                    [0,  0,  0],
+                                    [0,  0,   0]], 1).should =~ [{x:2, y:2}]
+    end
+
+    it "should recognize the NW corner with SE already blocked" do
+     AI.new.opposite_corner_moves([ [-1,  0,   0],
+                                    [0,  0,  0],
+                                    [0,  0,   1]], 1).should be_empty
+    end
+  end
+
+  describe "#empty_corner_moves" do
+    it "should return empty corners" do
+      AI.new.empty_corner_moves([ [0, 0, 0],
+                                  [0, 0, 0],
+                                  [0, 0, 0]], 1).should  =~ [{x:0, y:0}, 
+                                                              {x:2, y:0},
+                                                              {x:0, y:2},
+                                                              {x:2, y:2}]
+    end
+
+    it "should return empty corners" do
+      AI.new.empty_corner_moves([ [0, 0, 0],
+                                  [0, 0, 0],
+                                  [0, 0, 1]], 1).should  =~ [{x:0, y:0}, 
+                                                              {x:2, y:0},
+                                                              {x:0, y:2}]
+    end
+
+  end
+
+  describe "#empty_side_moves" do 
+    it "should return all open sides" do 
+      AI.new.empty_side_moves([ [0, 1, 0],
+                                  [0, 0, 0],
+                                  [0, 0, 0]], 1).should  =~ [{x:1, y:2}, 
+                                                              {x:2, y:1},
+                                                              {x:0, y:1}]
+    end
+  end
 end
