0a11ce0e2864661c788d247b6f544de69edc3989
Adam Wong <adamwong246@gmail.com>
2014-06-18 19:17:09 -0700

    it works, sort of

--- AI.rb
+++ AI.rb
@@ -2,6 +2,20 @@ require 'pry'
 
 class AI
 
+  def make_move(board, key)
+    (
+      winning_moves(board, key) +
+      blocking_moves(board, key) +
+      forking_moves(board, key) +
+      forking_moves(board, key) +
+      fork_blocking_moves(board, key) +
+      center_moves(board, key) +
+      opposite_corner_moves(board, key) +
+      empty_corner_moves(board, key) +
+      empty_side_moves(board, key) 
+    ).first
+  end
+
   def winning_moves(board, key)
 
     wins = []
@@ -84,9 +98,6 @@ class AI
         if board[y][x] == 0
           mutated_board = Marshal.load(Marshal.dump(board))
           mutated_board[y][x] = key
-
-          # binding.pry
-
           mutated_winning_moves = winning_moves(mutated_board, key).uniq
           if mutated_winning_moves.count > 1
             forking_moves << mutated_winning_moves
@@ -136,196 +147,5 @@ class AI
     moves << {x:1, y:2} if board[2][1] == 0
     moves << {x:1, y:0} if board[0][1] == 0
     moves
-  end
-
-
-end
-
-describe AI, "#score" do
-
-  describe "#winning_moves" do
-    it "should recognize a horizontal win" do
-      AI.new.winning_moves([[1,  1,  0],
-                            [0,  0,  0],
-                            [0,  0,  0]], 1).should include({x:2, y:0})
-    end
-
-    it "should recognize a vertical win" do
-      AI.new.winning_moves([[1,  0,  0],
-                            [1,  0,  0],
-                            [0,  0,  0]], 1).should include({x:0, y:2})
-    end
-    
-    it "should recognize a diagonal win" do
-      AI.new.winning_moves([[1,  0,  0],
-                            [0,  0,  0],
-                            [0,  0,  1]
-        ], 1).should include({x:1, y:1})
-    end
-
-    it "should recognize a reverse diagonal win" do
-      AI.new.winning_moves([[0,  0,  1],
-                            [0,  0,  0],
-                            [1,  0,  0]], 1).should include({x:1, y:1})
-    end
-
-    it "should recognize a not-win" do
-      AI.new.winning_moves([[1, 0, 0],
-                            [0, 0, 0],
-                            [0, 0, 0]],1).should be_empty
-    end
-
-    it "should recognize a not-win" do
-      AI.new.winning_moves([[1, 0, 0],
-                            [0, 0, 1],
-                            [0, 0, 0]],1).should be_empty
-    end
-
-    it "should recognize a not-win" do
-      AI.new.winning_moves([[0, 0, 0],
-                            [0, 0, 0],
-                            [0, 0, 0]],1).should be_empty
-    end
-  end
-
-  describe "#blocking_moves" do
-    it "should recognize a horizontal blocking_moves" do
-      AI.new.blocking_moves([[1,  1,  0],
-                            [0,  0,  0],
-                            [0,  0,  0]], -1).should include({x:2, y:0})
-    end
-
-    it "should recognize a vertical blocking_moves" do
-      AI.new.blocking_moves([[1,  0,  0],
-                            [1,  0,  0],
-                            [0,  0,  0]], -1).should include({x:0, y:2})
-    end
-
-    it "should recognize a diagonal blocking_moves" do
-      AI.new.blocking_moves([[1,  0,  0],
-                            [0,  0,  0],
-                            [0,  0,  1]], -1).should include({x:1, y:1})
-    end
-
-    it "should recognize a reverse diagonal blocking_moves" do
-      AI.new.blocking_moves([[0,  0,  1],
-                            [0,  0,  0],
-                            [1,  0,  0]], -1).should include({x:1, y:1})
-    end
-
-    it "should recognize a not-blocking_moves" do
-      AI.new.blocking_moves([[1, 0, 0],
-                            [0, 0, 0],
-                            [0, 0, 0]], -1).should be_empty
-    end
-  end
-
-  describe "#forking_moves" do
-    it "should recognize forking_moves" do
-      AI.new.forking_moves([[1,  0,  0],
-                            [0,  0,  0],
-                            [1,  0,  0]], 1).should include({x:2, y:0}, {x:1, y:1}, {x:2, y:2})
-    end
-
-    it "should recognize non-forking_moves" do
-      AI.new.forking_moves([[0,  0,  0],
-                            [0,  0,  0],
-                            [0,  0,  0]], 1).should be_empty
-    end
-  end
-
-  describe "#fork_blocking_moves" do
-    it "should recognize fork_blocking_moves" do
-      AI.new.fork_blocking_moves([[1,   0,  0],
-                                  [0,   0,  0],
-                                  [1,   0,  0]], -1).should include({x:2, y:0}, {x:1, y:1}, {x:2, y:2})
-    end
-
-    it "should recognize non-fork_blocking_moves" do
-      AI.new.fork_blocking_moves([[0,  0,  0],
-                                  [0,  0,  0],
-                                  [0,  0,  0]], -1).should be_empty
-    end
-  end
-
-  describe "#center_moves" do
-    it "should return the center square if available" do
-      AI.new.center_moves([[0,  0,  0],
-                            [0,  0,  0],
-                            [0,  0,  0]], 1).should eq([{x:1, y:1}])
-    end
-
-    it "should recognize when the center square is taken" do
-     AI.new.center_moves([ [0,  0,  0],
-                            [0,  1,  0],
-                            [0,  0,  0]], 1).should be_empty
-    end
-
-    it "should recognize when the center square is taken" do
-     AI.new.center_moves([ [0,  0,   0],
-                            [0,  -1,  0],
-                            [0,  0,   0]], 1).should be_empty
-    end
-  end
-
-  describe "#opposite_corner_moves" do
-     it "should recognize the NE corner" do
-     AI.new.opposite_corner_moves([ [0,  0, -1],
-                                    [0,  0,  0],
-                                    [0,  0,  0]], 1).should  =~ [{x:0, y:2}]
-    end
-
-    it "should recognize the SE corner" do
-     AI.new.opposite_corner_moves([ [0,  0,  0],
-                                    [0,  0,  0],
-                                    [0,  0, -1]], 1).should =~ [{x:0, y:0}]
-    end
-    it "should recognize the SW corner" do
-     AI.new.opposite_corner_moves([ [0,  0, 0],
-                                    [0,  0, 0],
-                                    [-1, 0, 0]], 1).should =~ [{x:2, y:0}]
-    end
-
-    it "should recognize the NW corner" do
-     AI.new.opposite_corner_moves([ [-1,  0,   0],
-                                    [0,  0,  0],
-                                    [0,  0,   0]], 1).should =~ [{x:2, y:2}]
-    end
-
-    it "should recognize the NW corner with SE already blocked" do
-     AI.new.opposite_corner_moves([ [-1,  0,   0],
-                                    [0,  0,  0],
-                                    [0,  0,   1]], 1).should be_empty
-    end
-  end
-
-  describe "#empty_corner_moves" do
-    it "should return empty corners" do
-      AI.new.empty_corner_moves([ [0, 0, 0],
-                                  [0, 0, 0],
-                                  [0, 0, 0]], 1).should  =~ [{x:0, y:0}, 
-                                                              {x:2, y:0},
-                                                              {x:0, y:2},
-                                                              {x:2, y:2}]
-    end
-
-    it "should return empty corners" do
-      AI.new.empty_corner_moves([ [0, 0, 0],
-                                  [0, 0, 0],
-                                  [0, 0, 1]], 1).should  =~ [{x:0, y:0}, 
-                                                              {x:2, y:0},
-                                                              {x:0, y:2}]
-    end
-
-  end
-
-  describe "#empty_side_moves" do 
-    it "should return all open sides" do 
-      AI.new.empty_side_moves([ [0, 1, 0],
-                                  [0, 0, 0],
-                                  [0, 0, 0]], 1).should  =~ [{x:1, y:2}, 
-                                                              {x:2, y:1},
-                                                              {x:0, y:1}]
-    end
   end
 end
---
+++ test.rb
@@ -0,0 +1,191 @@
+require 'pry'
+require_relative 'AI.rb'
+
+describe AI, "#score" do
+
+  describe "#winning_moves" do
+    it "should recognize a horizontal win" do
+      AI.new.winning_moves([[1,  1,  0],
+                            [0,  0,  0],
+                            [0,  0,  0]], 1).should include({x:2, y:0})
+    end
+
+    it "should recognize a vertical win" do
+      AI.new.winning_moves([[1,  0,  0],
+                            [1,  0,  0],
+                            [0,  0,  0]], 1).should include({x:0, y:2})
+    end
+    
+    it "should recognize a diagonal win" do
+      AI.new.winning_moves([[1,  0,  0],
+                            [0,  0,  0],
+                            [0,  0,  1]
+        ], 1).should include({x:1, y:1})
+    end
+
+    it "should recognize a reverse diagonal win" do
+      AI.new.winning_moves([[0,  0,  1],
+                            [0,  0,  0],
+                            [1,  0,  0]], 1).should include({x:1, y:1})
+    end
+
+    it "should recognize a not-win" do
+      AI.new.winning_moves([[1, 0, 0],
+                            [0, 0, 0],
+                            [0, 0, 0]],1).should be_empty
+    end
+
+    it "should recognize a not-win" do
+      AI.new.winning_moves([[1, 0, 0],
+                            [0, 0, 1],
+                            [0, 0, 0]],1).should be_empty
+    end
+
+    it "should recognize a not-win" do
+      AI.new.winning_moves([[0, 0, 0],
+                            [0, 0, 0],
+                            [0, 0, 0]],1).should be_empty
+    end
+  end
+
+  describe "#blocking_moves" do
+    it "should recognize a horizontal blocking_moves" do
+      AI.new.blocking_moves([[1,  1,  0],
+                            [0,  0,  0],
+                            [0,  0,  0]], -1).should include({x:2, y:0})
+    end
+
+    it "should recognize a vertical blocking_moves" do
+      AI.new.blocking_moves([[1,  0,  0],
+                            [1,  0,  0],
+                            [0,  0,  0]], -1).should include({x:0, y:2})
+    end
+
+    it "should recognize a diagonal blocking_moves" do
+      AI.new.blocking_moves([[1,  0,  0],
+                            [0,  0,  0],
+                            [0,  0,  1]], -1).should include({x:1, y:1})
+    end
+
+    it "should recognize a reverse diagonal blocking_moves" do
+      AI.new.blocking_moves([[0,  0,  1],
+                            [0,  0,  0],
+                            [1,  0,  0]], -1).should include({x:1, y:1})
+    end
+
+    it "should recognize a not-blocking_moves" do
+      AI.new.blocking_moves([[1, 0, 0],
+                            [0, 0, 0],
+                            [0, 0, 0]], -1).should be_empty
+    end
+  end
+
+  describe "#forking_moves" do
+    it "should recognize forking_moves" do
+      AI.new.forking_moves([[1,  0,  0],
+                            [0,  0,  0],
+                            [1,  0,  0]], 1).should include({x:2, y:0}, {x:1, y:1}, {x:2, y:2})
+    end
+
+    it "should recognize non-forking_moves" do
+      AI.new.forking_moves([[0,  0,  0],
+                            [0,  0,  0],
+                            [0,  0,  0]], 1).should be_empty
+    end
+  end
+
+  describe "#fork_blocking_moves" do
+    it "should recognize fork_blocking_moves" do
+      AI.new.fork_blocking_moves([[1,   0,  0],
+                                  [0,   0,  0],
+                                  [1,   0,  0]], -1).should include({x:2, y:0}, {x:1, y:1}, {x:2, y:2})
+    end
+
+    it "should recognize non-fork_blocking_moves" do
+      AI.new.fork_blocking_moves([[0,  0,  0],
+                                  [0,  0,  0],
+                                  [0,  0,  0]], -1).should be_empty
+    end
+  end
+
+  describe "#center_moves" do
+    it "should return the center square if available" do
+      AI.new.center_moves([[0,  0,  0],
+                            [0,  0,  0],
+                            [0,  0,  0]], 1).should eq([{x:1, y:1}])
+    end
+
+    it "should recognize when the center square is taken" do
+     AI.new.center_moves([ [0,  0,  0],
+                            [0,  1,  0],
+                            [0,  0,  0]], 1).should be_empty
+    end
+
+    it "should recognize when the center square is taken" do
+     AI.new.center_moves([ [0,  0,   0],
+                            [0,  -1,  0],
+                            [0,  0,   0]], 1).should be_empty
+    end
+  end
+
+  describe "#opposite_corner_moves" do
+    it "should recognize the NE corner" do
+     AI.new.opposite_corner_moves([ [0,  0, -1],
+                                    [0,  0,  0],
+                                    [0,  0,  0]], 1).should  =~ [{x:0, y:2}]
+    end
+
+    it "should recognize the SE corner" do
+     AI.new.opposite_corner_moves([ [0,  0,  0],
+                                    [0,  0,  0],
+                                    [0,  0, -1]], 1).should =~ [{x:0, y:0}]
+    end
+    it "should recognize the SW corner" do
+     AI.new.opposite_corner_moves([ [0,  0, 0],
+                                    [0,  0, 0],
+                                    [-1, 0, 0]], 1).should =~ [{x:2, y:0}]
+    end
+
+    it "should recognize the NW corner" do
+     AI.new.opposite_corner_moves([ [-1,  0,   0],
+                                    [0,  0,  0],
+                                    [0,  0,   0]], 1).should =~ [{x:2, y:2}]
+    end
+
+    it "should recognize the NW corner with SE already blocked" do
+     AI.new.opposite_corner_moves([ [-1,  0,   0],
+                                    [0,  0,  0],
+                                    [0,  0,   1]], 1).should be_empty
+    end
+  end
+
+  describe "#empty_corner_moves" do
+    it "should return empty corners" do
+      AI.new.empty_corner_moves([ [0, 0, 0],
+                                  [0, 0, 0],
+                                  [0, 0, 0]], 1).should  =~ [{x:0, y:0}, 
+                                                              {x:2, y:0},
+                                                              {x:0, y:2},
+                                                              {x:2, y:2}]
+    end
+
+    it "should return empty corners" do
+      AI.new.empty_corner_moves([ [0, 0, 0],
+                                  [0, 0, 0],
+                                  [0, 0, 1]], 1).should  =~ [{x:0, y:0}, 
+                                                              {x:2, y:0},
+                                                              {x:0, y:2}]
+    end
+
+  end
+
+  describe "#empty_side_moves" do 
+    it "should return all open sides" do 
+      AI.new.empty_side_moves([ [0, 1, 0],
+                                  [0, 0, 0],
+                                  [0, 0, 0]], 1).should  =~ [{x:1, y:2}, 
+                                                              {x:2, y:1},
+                                                              {x:0, y:1}]
+    end
+  end
+end
